2bf229b58a4548e9b05f1737250bc9c87ce0098b
Hthing <30700385+Hthing@users.noreply.github.com>
2019-08-13 22:03:37 +0800

    增加请求头UserAgent

--- yzw/settings.py
+++ yzw/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'yzw.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'yzw (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
